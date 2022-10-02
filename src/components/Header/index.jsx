5c2144239cdaf245e25bdc70eccfe593ea49c811
Hikkywannafly <76279331+TamNguyenS@users.noreply.github.com>
2022-10-02 17:06:03 +0700

    inital

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <>
+            <header >
+                <div className='px-2 z-40  h-40  top-0 left-0 w-full fixed bg-white bg-opacity-60 backdrop-blur fine-shadow'>
+                    <div className='bg-red-500 flex justify-between items-stretch'>
+                        <h1 className=' text-red-900 text-xl'> dsad</h1>
+                    </div>
+                </div>
+            </header>
+
+        </>
+    );
+}
+
+export default Header;
---
+++ src/page/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header';
+const Home = () => {
+    return (
+        <>
+            <div className='bg-gray-300 min-h-screen flex flex-col relative'>
+                <div class="absolute top-0 left-0 w-5 h-5"></div>
+                <Header />
+            </div>
+        </>
+    );
+}
+
+export default Home;
